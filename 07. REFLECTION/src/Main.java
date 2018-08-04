bfe412a63b7c736708e27f71f913be56938ef3a3
Bebrevski <stefan.bebrevski89@gmail.com>
2018-08-04 10:35:41 +0300

    Course exercises

--- 07. REFLECTION/src/Main.java
+++ 07. REFLECTION/src/Main.java
@@ -1,9 +1,9 @@
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
@@ -49,6 +49,28 @@ public class Main {
 
         // task 3 - High Quality Mistakes
 
-        
+        Arrays.stream(Reflection.class.getDeclaredFields())
+                .filter(f -> !Modifier.isPrivate(f.getModifiers()))
+                .sorted(Comparator.comparing(Field::getName))
+                .forEach(f -> System.out.println(String.format("%s must be private!"
+                        , f.getName())));
+
+        Arrays.stream(Reflection.class.getDeclaredMethods())
+                .filter(m -> m.getName().startsWith("get"))
+                .filter(m -> !m.getReturnType().equals(void.class))
+                .filter(m -> m.getParameterCount() == 0)
+                .filter(m -> !Modifier.isPublic(m.getModifiers()))
+                .sorted(Comparator.comparing(Method::getName))
+                .forEach(m -> System.out.println(String.format("%s have to be public!"
+                        , m.getName())));
+
+        Arrays.stream(Reflection.class.getDeclaredMethods())
+                .filter(m -> m.getName().startsWith("set"))
+                .filter(m -> m.getReturnType().equals(void.class))
+                .filter(m -> m.getParameterCount() == 1)
+                .filter(m -> !Modifier.isPrivate(m.getModifiers()))
+                .sorted(Comparator.comparing(Method::getName))
+                .forEach(m -> System.out.println(String.format("%s have to be private!"
+                        , m.getName())));
     }
 }
