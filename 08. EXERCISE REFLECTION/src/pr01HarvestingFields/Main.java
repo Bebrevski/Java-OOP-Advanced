728cf1f284ddd8754ee84b9336b3cbba1fcb4247
Bebrevski <stefan.bebrevski89@gmail.com>
2018-08-13 20:03:08 +0300

    Reflection exercises

--- 08. EXERCISE REFLECTION/src/pr01HarvestingFields/Main.java
+++ 08. EXERCISE REFLECTION/src/pr01HarvestingFields/Main.java
@@ -1,7 +1,31 @@
 package pr01HarvestingFields;
 
-public class Main {
-	public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
-	}
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String line;
+        try {
+            while (!"HARVEST".equals(line = reader.readLine())) {
+                String finalLine = line;
+                Arrays.stream(RichSoilLand.class.getDeclaredFields())
+                        .filter(f -> finalLine.equals("private") ?
+                                     Modifier.isPrivate(f.getModifiers()) :
+                                     finalLine.equals("protected") ?
+                                     Modifier.isProtected(f.getModifiers()) : !finalLine.equals("public") || Modifier.isPublic(f.getModifiers()))
+                        .forEach(f -> System.out.println(String.format("%s %s %s"
+                                , Modifier.toString(f.getModifiers())
+                                , f.getType().getSimpleName()
+                                , f.getName())));
+            }
+        } catch (IOException ex) {
+            System.out.println(Arrays.toString(ex.getStackTrace()));
+        }
+    }
 }
--- 08. EXERCISE REFLECTION/src/pr02PrivateClassFiddling/Main.java
+++ 08. EXERCISE REFLECTION/src/pr02PrivateClassFiddling/Main.java
@@ -1,8 +1,57 @@
 package pr02PrivateClassFiddling;
 
+import pr02PrivateClassFiddling.com.BlackBoxInt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-	}
+        String line;
+        try {
+
+            Constructor constructor = BlackBoxInt.class.getDeclaredConstructor(int.class);
+            constructor.setAccessible(true);
+
+            BlackBoxInt clazz = (BlackBoxInt) constructor.newInstance(0);
+            constructor.setAccessible(false);
+
+            while (!"END".equals(line = reader.readLine())) {
+                String[] tokens = line.split("_");
+                String methodName = tokens[0];
+                int value = Integer.parseInt(tokens[1]);
+
+                Method requestedMethod = clazz.getClass().getDeclaredMethod(methodName, int.class);
+                requestedMethod.setAccessible(true);
+
+                requestedMethod.invoke(clazz, value);
+
+                Field field = clazz.getClass().getDeclaredField("innerValue");
+
+                field.setAccessible(true);
+
+                System.out.println(field.get(clazz));
+
+                field.setAccessible(false);
+                requestedMethod.setAccessible(false);
+            }
+        } catch (
+                IOException |
+                        NoSuchMethodException |
+                        IllegalAccessException |
+                        InvocationTargetException |
+                        InstantiationException |
+                        NoSuchFieldException ex
+                ) {
+            ex.printStackTrace();
+        }
+    }
 }
